e7ad7df607483c822e778bdbd0f9b6094cba0b72
Afshan ahmed khan <afshanahmeda2k@gmail.com>
2022-03-01 14:46:01 +0530

    all exercises done

---
+++ exercises/standard_library_types/iterators3.rs
@@ -0,0 +1,113 @@
+// iterators3.rs
+// This is a bigger exercise than most of the others! You can do it!
+// Here is your mission, should you choose to accept it:
+// 1. Complete the divide function to get the first four tests to pass.
+// 2. Get the remaining tests to pass by completing the result_with_list and
+//    list_of_results functions.
+// Execute `rustlings hint iterators3` to get some hints!
+
+#[derive(Debug, PartialEq, Eq, Copy , Clone)]
+pub enum DivisionError {
+    NotDivisible(NotDivisibleError),
+    DivideByZero,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy , Clone)]
+pub struct NotDivisibleError {
+    dividend: i32,
+    divisor: i32,
+}
+
+// Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
+// Otherwise, return a suitable error.
+pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    }
+    else if a%b == 0 {
+        Ok(a/b)
+    }
+    else {
+        Err(DivisionError::NotDivisible(NotDivisibleError { dividend:a,divisor:b }))
+    }
+}
+
+// Complete the function and return a value of the correct type so the test passes.
+// Desired output: Ok([1, 11, 1426, 3])
+fn result_with_list() -> Result <[i32; 4], DivisionError> {
+//fn result_with_list() -> (){
+    let numbers = vec![27, 297, 38502, 81];
+    let mut arr:[i32;4] = [0;4];
+    for (n,i) in numbers.into_iter().enumerate() {
+        if let Ok(num) = divide(i,27) {
+            arr[n]=num;
+        } 
+    }
+    Ok(arr)
+    //println!("{}",division_results);
+}
+
+// Complete the function and return a value of the correct type so the test passes.
+// Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
+//fn list_of_results() -> () {
+fn list_of_results() -> [Result<i32,DivisionError>;4] {
+    let numbers = vec![27, 297, 38502, 81];
+    //let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results = numbers.into_iter().map(|n| divide(n,27)).filter(|res| res.is_ok());
+    let mut vec :Vec<Result<i32,DivisionError>> = Vec::new() ;
+    let mut arr :[Result<i32,DivisionError> ; 4] = [ Ok(0) , Ok(0) , Ok(0) , Ok(0) ,];
+    for (i,n) in division_results.into_iter().enumerate() {
+        //println!("{}",n);
+        //vec.push(Ok(n))
+        arr[i]=n;
+    }
+    //if let Ok(arr) = vec.try_into() {
+        //return arr;
+    //}
+    arr
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        assert_eq!(divide(81, 9), Ok(9));
+    }
+
+    #[test]
+    fn test_not_divisible() {
+        assert_eq!(
+            divide(81, 6),
+            Err(DivisionError::NotDivisible(NotDivisibleError {
+                dividend: 81,
+                divisor: 6
+            }))
+        );
+    }
+
+    #[test]
+    fn test_divide_by_0() {
+        assert_eq!(divide(81, 0), Err(DivisionError::DivideByZero));
+    }
+
+    #[test]
+    fn test_divide_0_by_something() {
+        assert_eq!(divide(0, 81), Ok(0));
+    }
+
+    #[test]
+    fn test_result_with_list() {
+        assert_eq!(format!("{:?}", result_with_list()), "Ok([1, 11, 1426, 3])");
+    }
+
+    #[test]
+    fn test_list_of_results() {
+        assert_eq!(
+            format!("{:?}", list_of_results()),
+            "[Ok(1), Ok(11), Ok(1426), Ok(3)]"
+        );
+    }
+}
